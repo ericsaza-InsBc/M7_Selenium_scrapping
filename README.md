74aac2ce92f541e699b887d1361d0338e3a71a18
Borja Montseny <borja.montseny@gmail.com>
2024-03-12 13:42:20 +0100

    Commit de Readme & Traducció

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# M7_Selenium_scrapping
+# M7 Python Selenium Book Scapper
+
+### Funcions
+
+##### `buscar_precio_libro(isbn)`
+
+Aquesta funció busca el preu d'un llibre a Agapea o a La Llar del Llibre depenent de la configuració de la variable `page`. Retorna un diccionari amb la informació rellevant sobre el llibre, com ara el preu, l'existència, el nom i l'ISBN.
+
+### Ús de la funció
+
+La funció `buscar_precio_libro(isbn)` s'ha utilitzat per a cercar el preu de dos llibres amb ISBNs específics, que es mostren com a exemple a la darrera part del codi.
+
+## Execució del codi
+
+Per executar el codi, assegureu-vos d'instal·lar les dependències del projecte i tenir un navegador web compatible amb Selenium configurat correctament.
+
+```bash
+pip install selenium
--- eric_selenium_scrapping.py
+++ eric_selenium_scrapping.py
@@ -5,65 +5,65 @@ import time
 
 page = "llardelllibre"
 
-# Función que busca el precio de un libro en Agapea
+# Funció que busca el preu d'un llibre a Agapea
 def buscar_precio_libro(isbn):
 
-    # Instanciamos un driver
+    # Instanciem un driver
     driver = webdriver.Firefox()
 
-    # Maximizamos la ventana
+    # Maximizem la finestra
     driver.maximize_window()
 
-    # Abre la página necesaria
+    # Obre la pàgina necessària
     if (page == "agapea"):
         driver.get(f"https://www.agapea.com/poesies-{isbn}-i.htm")
 
-        # Espera a que la página cargue
+        # Espera que la pàgina carregui
         time.sleep(4)
 
-        # Aceptamos las cookies
+        # Acceptem les cookies
         driver.find_element(By.XPATH, "//button[@id='cookie-accept']").click()
         
-        # Espera a que se quite el alert
+        # Espera que desaparegui l'alerta
         time.sleep(4)
 
-        # Obtenemos el nombre
+        # Obtenim el nom
         nombre_producto = driver.find_element(By.XPATH, "//div[@class='datos-libro']/div/h1").text
 
-        # Obtenemos el precio
+        # Obtenim el preu
         precio_producto = driver.find_element(By.XPATH, "//strong[1]").text.replace("€", "").replace(",", ".").strip()
 
-        # Obtenemos el ISBN13
+        # Obtenim l'ISBN13
         isbn13 = driver.find_element(By.XPATH, "//tbody/tr[6]/td[1]/b[1]").text
 
-        # Obtenemos el elemento de las existencias
+        # Obtenim l'element de les existencias
         existencias = 'no available' if driver.find_element(By.CSS_SELECTOR, "div[class='etiqueta']").text == "SIN EXISTENCIAS" else 'available'
 
     elif (page == "llardelllibre"):
         driver.get(f"https://www.llardelllibre.cat/cat/")
 
-        # Espera a que la página cargue
+        # Espera que la pàgina carregui
         time.sleep(4)
 
-        # Escribimos el ISBN en el buscador
+        # Escrivim l'ISBN al cercador
         driver.find_element(By.XPATH, "//input[@placeholder='Títol, autor o ISBN']").send_keys(isbn)
 
-        # Hacemos click en el botón de buscar
+        # Cliquem al botó de cercar
         driver.find_element(By.XPATH, "//button[@title='Buscar']").click()
         
-        # Obtenemos el nombre
+        # Obtenim el nom
         nombre_producto = driver.find_element(By.XPATH, "//dd[@class='title']/a").text
         
-        # Obtenemos el precio
+        # Obtenim el preu
         precio_producto = driver.find_element(By.CSS_SELECTOR, "p[class='precio'] strong").text.replace("€", "").replace(",", ".").strip()
 
-        # Obtenemos el ISBN13
+        # Obtenim l'ISBN13
         isbn13 = isbn
 
-        # Obtenemos el elemento de las existencias
+        # Obtenim l'element de les existencias
         existencias = 'no available' if driver.find_element(By.XPATH, "//dd[@class='disponibilidad orange']").text == "Sense estoc, consultar termini entrega" else 'available'
 
-    # Creamos un diccionario con la información
+    # Creem un diccionari amb la informació
     infoLlibre = {  
         "EAN13": isbn13,
         "ecommerce": page,
@@ -72,14 +72,14 @@ def buscar_precio_libro(isbn):
         "price": None if existencias == 'no available' else precio_producto
     }
 
-    # Imprimimos la información
+    # Imprimim
     print(infoLlibre)
 
-    # Cerramos el navegador
+    # Tanquem el navegador
     driver.quit()
 
 
-# Uso de la función con un ISBN específico
+# Ús de la funció amb un ISBN específic
 isbn = "9788418928949"
 isbn2 = "9788482649498"
 buscar_precio_libro(isbn)
